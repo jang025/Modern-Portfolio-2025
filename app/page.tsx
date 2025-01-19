a55eb5688f56c7940601bb6b1dc544e5d106ec79
Jeremy Ang <jang025@users.noreply.github.com>
2025-01-19 15:41:52 +0800

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Hero from "@/components/Hero";
+import FloatingNavBar from "@/components/ui/FloatingNavBar";
+import { navItems } from "@/data";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Hello, Portfolio!</h1>
+        <FloatingNavBar navItems={navItems} />
+        <Hero />
       </div>
     </main>
   );
---
+++ data/index.ts
@@ -0,0 +1,85 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/tail.svg", "/re.svg", "/three.svg", "/gsap.svg"],
+    link: "apple-website-six-inky.vercel.app",
+  },
+  {
+    id: 2,
+    title: "Movie Application",
+    des: "Movie Application that renders list of movies , along with their movie information from the OMDb API",
+    img: "/Movie-Production.png",
+    iconLists: ["/re.svg"],
+    link: "movie-app-five-lime.vercel.app",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based dashboard using React.js",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/jang025",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/jeremy-ang-pin-en",
+  },
+  {
+    id: 3,
+    img: "/resume.png",
+    link: "https://drive.google.com/file/d/1hZVbDNZkXvSW22xTbs1bE3P_zH0ue-PT/view?usp=sharing",
+  },
+];
